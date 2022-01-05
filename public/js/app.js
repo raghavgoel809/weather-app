5dafb7a8cbfc9c074a733f05b4b766a1677078aa
raghav <raghavgoel809@gmail.com>
2022-01-06 00:19:42 +0530

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,15 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit',(e) => {
+
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
-    messageOne.textContent = 'Loading...'
+    messageOne.textContent = "loading..."
     messageTwo.textContent = ''
-    fetch(('/weather?address=' + location).then((response) => {
+
+    fetch(('/weather?address=' + location).then((response)=> {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
@@ -22,3 +24,6 @@ weatherForm.addEventListener('submit',(e) => {
     })
 }))
 })
+
+
+
